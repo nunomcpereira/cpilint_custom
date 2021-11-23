0960ecd3f3712f79a5732010dd85863c4e0c895c
Administrator <konkering@gmail.com>
2021-11-23 13:55:29 +0000

    Changes to the unused parameter rule to cover a more sophisticaed exclusion list (by iflow)

--- src/com/nmp/cpilint/impl/UnusedParametersRule.java
+++ src/com/nmp/cpilint/impl/UnusedParametersRule.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -17,6 +19,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
@@ -30,15 +34,22 @@ final class UnusedParametersRule extends RuleBase {
 
 	private List<String> exclusionList;
 
+	private IflowArtifactTag currentTagBeingAnalysed = null;
 	private Map<String, String> fileNameFileContentCache = new HashMap<>();
 
 	public UnusedParametersRule(List<String> exclusionList) {
 		this.exclusionList = exclusionList == null ? Collections.emptyList() : exclusionList;
+		System.out.println(String.format("Unused rule created with the following exclusion list [%s]",
+				String.join(",", exclusionList)));
 	}
 
 	@Override
 	public void inspect(IflowArtifact iflow) {
 		IflowArtifactTag tag = iflow.getTag();
+		if (!tag.equals(currentTagBeingAnalysed)) {
+			fileNameFileContentCache = new HashMap<>();
+			currentTagBeingAnalysed = tag;
+		}
 		Collection<ArtifactResource> externalParameters = iflow
 				.getResourcesByType(ArtifactResourceType.EXTERNAL_PARAMETERS);
 		for (ArtifactResource externalParam : externalParameters) {
@@ -51,21 +62,54 @@ final class UnusedParametersRule extends RuleBase {
 				Document contents = loadXMLFromString(fileContentsStr);
 				NodeList nodeList = contents.getFirstChild().getChildNodes();
 				for (int i = 0; i < nodeList.getLength(); i++) {
-					NodeList parameterFields = nodeList.item(i).getChildNodes();
+					Node item = nodeList.item(i);
+					String paramNameElement = "name";
 					String externalParamName = "";
-					for (int y = 0; y < parameterFields.getLength(); y++) {
-						if ("name".equalsIgnoreCase(parameterFields.item(y).getNodeName())) {
-							externalParamName = parameterFields.item(y).getTextContent();
-							break;
+					if (item.getNodeName().equalsIgnoreCase("param_references")) {
+						if (item.getFirstChild() == null) {
+							continue;
+						}
+						paramNameElement = "param_key";
+						NamedNodeMap attributes = item.getChildNodes().item(0).getAttributes();
+						for (int y = 0; y < attributes.getLength(); y++) {
+							if (paramNameElement.equalsIgnoreCase(attributes.item(y).getNodeName())) {
+								externalParamName = attributes.item(y).getTextContent();
+								break;
+							}
+						}
+					} else {
+						NodeList parameterFields = item.getChildNodes();
+
+						for (int y = 0; y < parameterFields.getLength(); y++) {
+							if (paramNameElement.equalsIgnoreCase(parameterFields.item(y).getNodeName())) {
+								externalParamName = parameterFields.item(y).getTextContent();
+								break;
+							}
 						}
 					}
-					if (!"".equals(externalParamName) && !exclusionList.contains(externalParamName)) {
-						boolean used = isExternalParameterBeingUsed(externalParamName, iflow);
-						if (!used) {
-							consumer.consume(new UnusedParameterIssue(tag,
-									String.format("External parameter %s not being used", externalParamName)));
+					if ("".equals(externalParamName)) {
+						if (item.getNodeName().equalsIgnoreCase("param_references") && item.getFirstChild() != null) {
+							System.err.println("Parameter not found for " + item + " on iflow" + tag);
 						}
+						continue;
 					}
+					if (exclusionList.contains(externalParamName)) {
+						System.out.println(String.format("Parameter [%s] is globally excluded from exclusion list [%s]",
+								externalParamName, String.join(", ", exclusionList)));
+						continue;
+					}
+					if (exclusionList.contains(tag.getName() + ":" + externalParamName)) {
+						System.out.println(String.format("Parameter [%s] is iflow excluded from exclusion list [%s]",
+								externalParamName, String.join(", ", exclusionList)));
+						continue;
+					}
+
+					boolean used = isExternalParameterBeingUsed(externalParamName, iflow);
+					if (!used) {
+						consumer.consume(new UnusedParameterIssue(tag,
+								String.format("External parameter [%s] not being used", externalParamName)));
+					}
+
 				}
 			} catch (Exception e) {
 				System.err.println("Exception: [" + e.getLocalizedMessage() + "]");
@@ -96,7 +140,8 @@ final class UnusedParametersRule extends RuleBase {
 		String patternString = ".*\\b" + externalParamName + "\\b.*";
 		Pattern pattern = Pattern.compile(patternString);
 		for (ArtifactResourceType entry : ArtifactResourceType.values()) {
-			if (entry.equals(ArtifactResourceType.EXTERNAL_PARAMETERS)) {
+			if (entry.equals(ArtifactResourceType.EXTERNAL_PARAMETERS) || entry.equals(ArtifactResourceType.EDMX)
+					|| entry.equals(ArtifactResourceType.WSDL)) {
 				continue;
 			}
 			Collection<ArtifactResource> resources = iflow.getResourcesByType(entry);
@@ -112,6 +157,7 @@ final class UnusedParametersRule extends RuleBase {
 				if (matcher.find()) {
 					return true;
 				}
+
 			}
 		}
 		return false;
