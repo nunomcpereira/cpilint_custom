8a2dd2b9fa32b48ba34652f7ad9e771e27127201
Administrator <konkering@gmail.com>
2023-02-07 17:06:16 +0000

    New csrf protection rule allowing exemptions

---
+++ src/com/nmp/cpilint/impl/CsrfProtectionRequiredWithExcludeRule.java
@@ -0,0 +1,51 @@
+package com.nmp.cpilint.impl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+import dk.mwittrock.cpilint.IflowXml;
+import dk.mwittrock.cpilint.artifacts.IflowArtifact;
+import dk.mwittrock.cpilint.artifacts.IflowArtifactTag;
+import dk.mwittrock.cpilint.consumers.IssueConsumer;
+import dk.mwittrock.cpilint.issues.CsrfProtectionRequiredIssue;
+import dk.mwittrock.cpilint.issues.Issue;
+import dk.mwittrock.cpilint.model.SenderAdapter;
+import dk.mwittrock.cpilint.model.XmlModel;
+import dk.mwittrock.cpilint.model.XmlModelFactory;
+import dk.mwittrock.cpilint.rules.RuleBase;
+import dk.mwittrock.cpilint.rules.XpathRulesUtil;
+import net.sf.saxon.s9api.XdmNode;
+
+final class CsrfProtectionRequiredWithExcludeRule extends RuleBase {
+	private List<String> exclusionList;
+	private List<String> inclusionList;
+	private boolean exclusionMode = true;
+	
+	public CsrfProtectionRequiredWithExcludeRule(List<String> exclusionList, List<String> inclusionList) {
+		this.exclusionList = exclusionList == null ? Collections.emptyList() : exclusionList;
+		this.inclusionList = inclusionList == null ? Collections.emptyList() : inclusionList;
+		if (this.exclusionList.size() != 0) {
+			exclusionMode = true;
+			System.out.println(String.format("CSRF Protection Required following exclusion list [%s]",
+					String.join(",", exclusionList)));
+		}
+		if (this.inclusionList.size() != 0) {
+			exclusionMode = false;
+			System.out.println(String.format("CSRF Protection Required following inclusion list [%s]",
+					String.join(",", inclusionList)));
+		}
+	}
+
+	@Override
+	public void inspect(IflowArtifact iflow) {
+		IflowXml iflowXml = iflow.getIflowXml();
+		XmlModel model = XmlModelFactory.getModelFor(iflowXml);
+		IflowArtifactTag tag = iflow.getTag();
+		IssueConsumer exclusionInclusionConsumer = new ExclusionInclusionIssueConsumer(exclusionList,inclusionList,tag.getId(),exclusionMode);
+		String noCsrfChannelsXpath = model.xpathForSenderChannels(SenderAdapter.HTTPS, model.channelPredicateForNoCsrfProtection());
+		Function<XdmNode, Issue> issueFunction = n -> new CsrfProtectionRequiredIssue(tag, model.getChannelNameFromElement(n), model.getChannelIdFromElement(n));
+		XpathRulesUtil.iterateSingleXpathAndConsumeIssues(iflowXml, noCsrfChannelsXpath, issueFunction, exclusionInclusionConsumer::consume);
+	}
+
+}
---
+++ src/com/nmp/cpilint/impl/CsrfProtectionRequiredWithExcludeRuleFactory.java
@@ -0,0 +1,34 @@
+package com.nmp.cpilint.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.dom4j.Element;
+
+import dk.mwittrock.cpilint.rules.Rule;
+import dk.mwittrock.cpilint.rules.RuleFactory;
+import dk.mwittrock.cpilint.rules.RuleFactoryError;
+
+public final class CsrfProtectionRequiredWithExcludeRuleFactory implements RuleFactory {
+
+	@Override
+	public boolean canCreateFrom(Element e) {
+		return e.getName().equals("csrf-protection-required-with-exclude");
+	}
+
+	@Override
+	public Rule createFrom(Element e) {
+		if (!canCreateFrom(e)) {
+			throw new RuleFactoryError(String.format("Cannot create Rule object from element '%s'", e.getName()));
+		}
+		List<String> exclusions = e.elements("exclude").stream().map(n -> n.getText()).collect(Collectors.toList());
+		List<String> inclusions = e.elements("include").stream().map(n -> n.getText()).collect(Collectors.toList());
+		if (inclusions.size() > 0 && exclusions.size() > 0) {
+			throw new RuleFactoryError(String.format(
+					"Cannot create Rule with both exclusions and inclusions. Select only one mode. object from element '%s'",
+					e.getName()));
+		}
+		return new CsrfProtectionRequiredWithExcludeRule(exclusions, inclusions);
+	}
+
+}
